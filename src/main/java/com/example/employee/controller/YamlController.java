c73536e450b08da88c899373ce68c9ec1f813457
Mosin1425 <2019pietcsmosin99@poornima.org>
2023-10-18 10:16:46 +0530

    Updated Employees.yaml and YamlController.java files

--- src/main/java/com/example/employee/controller/YamlController.java
+++ src/main/java/com/example/employee/controller/YamlController.java
@@ -11,11 +11,12 @@ import org.kohsuke.github.GHContentUpdateResponse;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
@@ -58,18 +59,18 @@ public class YamlController {
         }
     }
 	
-	@PutMapping("/update-file-remotely")
+	@PutMapping("/add-new-key-value")
 	public String updateRemote(@RequestBody Map<String, String> keyValue) {
 	    try {
-	        // Replace with your personal access token
-	        String personalAccessToken = "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";
+	        // Personal Access Token
+	        String personalAccessToken = "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";
 
 	        // Initialize GitHub client
 	        GitHub gitHub = new GitHubBuilder()
 	                .withOAuthToken(personalAccessToken)
 	                .build();
 
-	        // Specify your GitHub repository and the path to the YAML file
+	        // GitHub Repository Path URL
 	        String repoOwner = "Mosin1425";
 	        String repoName = "Employee-Incture";
 	        String filePath = "src/main/resources/Employees.yaml";
@@ -79,30 +80,30 @@ public class YamlController {
 	        GHContent content = repo.getFileContent(filePath);
 	        @SuppressWarnings("deprecation")
 			String existingYamlContent = content.getContent();
-
+	        
 	        Yaml yaml = new Yaml();
 	        Map<String, Object> yamlMap = yaml.load(existingYamlContent);
-	        
+
 	        Map.Entry<String, String> entry = keyValue.entrySet().iterator().next();
 	        String newKey = entry.getKey();
 	        String newValue = entry.getValue();
-	        yamlMap.put(newKey, newValue);
-	        // Modify the YAML content with the new key-value pair
-//	        String updatedYamlContent = existingYamlContent +
-//	            "\n" + newKey + ": " + newValue;
 	        
+	        if(yamlMap.containsKey(newKey)) {
+	        	return "The key " + newKey + " is already present in the file";
+	        }
+	        yamlMap.put(newKey, newValue);
+	       
 	        DumperOptions options = new DumperOptions();
 	        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
 	        Yaml updatedYaml = new Yaml(options);
 	        String updatedYamlContent = updatedYaml.dump(yamlMap);
-
-	        // Construct the commit message
+	        
+	        
 	        String commitMessage = "Update YAML file with new key as : " + newKey + " and value as : " + newValue;
 
 	        // Update the file on GitHub
 	        GHContentUpdateResponse updateResponse = content.update(updatedYamlContent, commitMessage);
 
-	        // Check if the update was successful
 	        if (updateResponse != null) {
 	            return "YAML file updated successfully.";
 	        } else {
@@ -115,4 +116,136 @@ public class YamlController {
 	    }
 	}
 	
+	@DeleteMapping("/delete-by-key/{key}")
+	public String deleteRemotely(@PathVariable String key) {
+		try {
+	        String personalAccessToken = "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";
+	        GitHub gitHub = new GitHubBuilder()
+	                .withOAuthToken(personalAccessToken)
+	                .build();
+
+	        // GitHub repository URL to YAML file
+	        String repoOwner = "Mosin1425";
+	        String repoName = "Employee-Incture";
+	        String filePath = "src/main/resources/Employees.yaml";
+
+	        // Fetch the existing YAML file content
+	        GHRepository repo = gitHub.getRepository(repoOwner + "/" + repoName);
+	        GHContent content = repo.getFileContent(filePath);
+	        @SuppressWarnings("deprecation")
+			String existingYamlContent = content.getContent();
+	        
+	        Yaml yaml = new Yaml();
+	        Map<String, Object> yamlMap = yaml.load(existingYamlContent);
+	        
+	        if(yamlMap.containsKey(key)) {
+	        	yamlMap.remove(key);
+	        	
+	        	DumperOptions options = new DumperOptions();
+		        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+		        Yaml updatedYaml = new Yaml(options);
+		        String updatedYamlContent = updatedYaml.dump(yamlMap);
+		        
+		        String commitMessage = "Delete key named as " + key;
+		        GHContentUpdateResponse updateResponse = content.update(updatedYamlContent, commitMessage);
+		        
+		        if (updateResponse != null) {
+		        	return "Key deleted successfully...";
+		        } else {
+		            return "Failed to delete the key...";
+		        }
+	        }
+	        else {
+	        	return "Key doesn't exist";
+	        }
+	        
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Error deleting the file " + e.getMessage();
+		}
+	}
+	
+	@PutMapping("/update-key")
+	public String updateKey(@RequestBody Map<String, String> keyValue) {
+		try {
+			String personalAccessToken = "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";
+	        GitHub gitHub = new GitHubBuilder()
+	                .withOAuthToken(personalAccessToken)
+	                .build();
+
+	        // GitHub repository URL to YAML file
+	        String repoOwner = "Mosin1425";
+	        String repoName = "Employee-Incture";
+	        String filePath = "src/main/resources/Employees.yaml";
+
+	        // Fetch the existing YAML file content
+	        GHRepository repo = gitHub.getRepository(repoOwner + "/" + repoName);
+	        GHContent content = repo.getFileContent(filePath);
+	        @SuppressWarnings("deprecation")
+			String existingYamlContent = content.getContent();
+	        
+	        Yaml yaml = new Yaml();
+	        Map<String, Object> yamlMap = yaml.load(existingYamlContent);
+	        
+	        Map.Entry<String, String> entry = keyValue.entrySet().iterator().next();
+	        String newKey = entry.getKey();
+	        String newValue = entry.getValue();
+	        
+	        if(yamlMap.containsKey(newKey)) {
+	        	yamlMap.put(newKey, newValue);
+	        	
+	        	DumperOptions options = new DumperOptions();
+		        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+		        Yaml updatedYaml = new Yaml(options);
+		        String updatedYamlContent = updatedYaml.dump(yamlMap);
+		        
+		        String commitMessage = "Key: " + newKey + " updated successfully...";
+		        GHContentUpdateResponse updateResponse = content.update(updatedYamlContent, commitMessage);
+		        
+		        if (updateResponse != null) {
+		        	return "Key updated successfully...";
+		        } else {
+		            return "Failed to delete the key...";
+		        }
+	        }
+	        else {
+	        	return "Key doesn't exist";
+	        }
+	        
+	        
+	        
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Error updating the file..." + e.getMessage();
+		}
+	}
+	
+	@GetMapping("/get-from-file-github")
+	public Map<String, Object> getFromGithub() {
+		try {
+			String personalAccessToken = "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";
+	        GitHub gitHub = new GitHubBuilder()
+	                .withOAuthToken(personalAccessToken)
+	                .build();
+
+	        // GitHub repository URL to YAML file
+	        String repoOwner = "Mosin1425";
+	        String repoName = "Employee-Incture";
+	        String filePath = "src/main/resources/Employees.yaml";
+
+	        // Fetch the existing YAML file content
+	        GHRepository repo = gitHub.getRepository(repoOwner + "/" + repoName);
+	        GHContent content = repo.getFileContent(filePath);
+	        @SuppressWarnings("deprecation")
+			String existingYamlContent = content.getContent();
+	        
+	        Yaml yaml = new Yaml();
+	        Map<String, Object> yamlMap = yaml.load(existingYamlContent);
+	        
+	        return yamlMap;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
